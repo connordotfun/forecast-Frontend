57b420eb46080aba53cae3533266e30f5da90fcf
Connor Hudson <cohu3291@colorado.edu>
2018-04-20 08:31:29 -0600

    Align contents of InfoCard properly

--- src/components/InfoCard/index.tsx
+++ src/components/InfoCard/index.tsx
@@ -17,8 +17,8 @@ const InfoCard: React.SFC<InfoCardProps> = (props) => {
                 <WeatherIcon icon={props.data.weather.weather[0].icon} />
             </div>
             <div className="region">
-                <h3>{props.data.region.name}</h3>
-                <p>Sentiment: {props.data.sentiment.toFixed(5)}</p>
+                <h3 className="region-name">{props.data.region.name}</h3>
+                <p className="sentiment">Sentiment: {props.data.sentiment.toFixed(5)}</p>
             </div>
             {/* {props.expanded && 
                 <Tweet tweetId={props.data.tid} />
