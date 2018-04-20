97575c893d9b85ccbe0058f28226a4cfe48bca9c
Connor Hudson <cohu3291@colorado.edu>
2018-04-20 15:07:39 -0600

    Expanded map for sidebar cards

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -7,6 +7,7 @@ import InfoCard from '../../components/InfoCard'
 import './index.css'
 import logo from '../../assets/logo.png'
 import Region from '../../models/Region';
+import { observable } from 'mobx';
 
 interface SidebarProps {
     messageStore?: MessageStore
@@ -16,15 +17,19 @@ interface SidebarProps {
 @inject('messageStore')
 @observer
 class Sidebar extends React.Component<SidebarProps> {
-
+    @observable private _$expandedCards: Map<string, boolean> = new Map()
     render() {
-        let availableRegions: number = 0
         let cards: React.ReactNode[] = []
         if (this.props.messageStore) {
                 this.props.messageStore.$latestMessages.forEach(
                     (region, id) => (
                         cards.push(
-                            <InfoCard key={id} data={region} onClick={() => this._zoomToRegion(region.region)}/>
+                            <InfoCard
+                                key={id}
+                                data={region}
+                                onClick={() => this._zoomToRegion(region.region)}
+                                expanded={this._$expandedCards.get(id)}
+                            />
                         )
                     )
                 )
@@ -32,7 +37,7 @@ class Sidebar extends React.Component<SidebarProps> {
         return(
             <div className="sidebar">
                 <img src={logo} alt="FORECAST" className="branding"/>
-                <div className="region-cards" key={availableRegions}>
+                <div className="region-cards">
                     {cards}
                 </div>
             </div>
@@ -40,6 +45,7 @@ class Sidebar extends React.Component<SidebarProps> {
     }
 
     private _zoomToRegion(region: Region) {
+        this._$expandedCards.set(region.ID, !this._$expandedCards.get(region.ID))
         if (this.props.google) {
             this.props.google.map.setZoom(10)
             this.props.google.map.setCenter({lat: region.centerLat, lng: region.centerLon})
