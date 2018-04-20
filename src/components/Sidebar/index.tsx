05d13e8d09be936b7d3000f0af23d24362d9a577
Connor Hudson <cohu3291@colorado.edu>
2018-04-19 23:39:40 -0600

    Display InfoCards in sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react'
-// import Message from '../../models/Message';
 import { MessageStore } from '../../stores/messageStore';
 import { observer, inject } from 'mobx-react';
 
+import InfoCard from '../../components/InfoCard'
+
 import './index.css'
 import logo from '../../assets/logo.png'
+import { observable } from 'mobx';
 
 interface SidebarProps {
     messageStore?: MessageStore
@@ -13,10 +15,32 @@ interface SidebarProps {
 @inject('messageStore')
 @observer
 class Sidebar extends React.Component<SidebarProps> {
+    @observable cards: React.ReactNode[]
+
+    componentWillReact() {
+        const messages = this.props.messageStore ? this.props.messageStore.$latestMessages : false
+        if (messages) {
+            let tempCards: React.ReactNode[] = []
+            messages.forEach(
+                (region, id) => (
+                    tempCards.push(<InfoCard key={id} data={region} />)
+                )
+            )
+            this.cards = tempCards
+        }
+    }
+
     render() {
+        let availableRegions: number = 0
+        if (this.props.messageStore) {
+            availableRegions = this.props.messageStore.$latestMessages.size
+        }
         return(
             <div className="sidebar">
                 <img src={logo} alt="FORECAST" className="branding"/>
+                <div className="region-cards" key={availableRegions}>
+                    {this.cards}
+                </div>
             </div>
         )
     }
