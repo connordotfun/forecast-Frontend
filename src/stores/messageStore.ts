826e586ff236773f8c9d7b0b960892d5472a907c
Connor Hudson <cohu3291@colorado.edu>
2018-04-20 13:03:18 -0600

    Track sentiment, temperature deltas

--- src/stores/messageStore.ts
+++ src/stores/messageStore.ts
@@ -1,11 +1,17 @@
 import { observable, action } from 'mobx'
 import Message from '../models/Message';
-
+// tslint:disable no-console
 export class MessageStore {
     @observable $latestMessages: Map<string, Message> = new Map()
 
     @action
     public addMessage(message: Message): void {
+        let prev = this.getMessage(message.ID)
+        if (prev) {
+            let sDiff = message.sentiment - prev.sentiment
+            let tDiff = message.weather.main.temp - prev.weather.main.temp
+            console.log(`${message.region.name} update: ΔS = ${sDiff}, ΔT = ${tDiff}`)
+        }
         this.$latestMessages.set(message.ID, message)
     }
 
