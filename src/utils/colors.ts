c9b68ff744038d7555112bb90b99a447ae142561
Connor Hudson <cohu3291@colorado.edu>
2018-04-28 17:56:11 -0600

    Make colors more colorblindness-friendly

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -2,10 +2,10 @@ import { scale } from 'chroma-js'
 
 const COLORS = [
     '#ff206e',
-    '#2ca58d'
+    '#24cdd6'
 ]
 
 export const sentimentHex = (value: number) => {
-    const gradient = scale(COLORS).mode('rgb')
-    return gradient((value + 1) / 2).hex()
+    const gradient = scale(COLORS).mode('rgb').domain([-1, 1])
+    return gradient(value).hex()
 }
