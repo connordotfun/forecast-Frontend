b57918a1852e771b5ea2913cf5eb3947bca9c6c1
Connor Hudson <cohu3291@colorado.edu>
2018-04-29 17:06:28 -0600

    Tweak colors, update message model

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -2,10 +2,10 @@ import { scale } from 'chroma-js'
 
 const COLORS = [
     '#ff206e',
-    '#24cdd6'
+    '#11dee8'
 ]
 
 export const sentimentHex = (value: number) => {
-    const gradient = scale(COLORS).mode('rgb').domain([-1, 1])
+    const gradient = scale(COLORS).mode('rgb').domain([-1, 0.75])
     return gradient(value).hex()
 }
