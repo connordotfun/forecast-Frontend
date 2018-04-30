6b61420a09cbfcbbdda0313c094dc1426141f1b2
Connor Hudson <cohu3291@colorado.edu>
2018-04-29 21:04:24 -0600

    add sort by tweet count method to sorting

--- src/utils/sorting.ts
+++ src/utils/sorting.ts
@@ -4,6 +4,14 @@ const sentimentSort = (a: [string, Message], b: [string, Message]) => (
     Math.abs(b[1].sentiment) - Math.abs(a[1].sentiment)
 )
 
+const countSort = (a: [string, Message], b: [string, Message]) => (
+    b[1].tid.length - a[1].tid.length
+)
+
 export const sortMapBySentiment = (map: Map<string, Message>) => (
     new Map(Array.from(map.entries()).sort(sentimentSort))
+)
+
+export const sortMapByTweetCount = (map: Map<string, Message>) => (
+    new Map(Array.from(map.entries()).sort(countSort))
 )
